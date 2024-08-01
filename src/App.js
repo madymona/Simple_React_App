05ecf0d6649fb125233a3e1d9cc02ef7d844cb8b
madymona <madymona@gmail.com>
2024-08-01 15:50:40 -0400

    Changes done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header";
+import Content from "./Components/Content";
+import Footer from "./Components/Footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Content color="blue" text="This is my first React Application!" />
+      <Content color="red" text="Wish me luck..." />
+      <Content color="green" text="I think I've got it!" />
+      <Footer />
+    </>
   );
 }
 
